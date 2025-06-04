084e2ff069dbe9b8c136c3c5e7e6ff4b29ff94a5
Zach <zblixt4@gmail.com>
2025-06-04 10:28:51 -0700

    UNet

---
+++ UNet.py
@@ -0,0 +1,57 @@
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, concatenate, Dropout
+
+def unet(input_size=(256, 256, 3), num_classes=1):
+    inputs = Input(input_size)
+
+    # Encoder
+    c1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    c1 = Conv2D(64, (3, 3), activation='relu', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(128, (3, 3), activation='relu', padding='same')(p1)
+    c2 = Conv2D(128, (3, 3), activation='relu', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(256, (3, 3), activation='relu', padding='same')(p2)
+    c3 = Conv2D(256, (3, 3), activation='relu', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(512, (3, 3), activation='relu', padding='same')(p3)
+    c4 = Conv2D(512, (3, 3), activation='relu', padding='same')(c4)
+    p4 = MaxPooling2D((2, 2))(c4)
+
+    c5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(p4)
+    c5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(c5)
+
+    # Decoder
+    u6 = UpSampling2D((2, 2))(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(512, (3, 3), activation='relu', padding='same')(u6)
+    c6 = Conv2D(512, (3, 3), activation='relu', padding='same')(c6)
+
+    u7 = UpSampling2D((2, 2))(c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(256, (3, 3), activation='relu', padding='same')(u7)
+    c7 = Conv2D(256, (3, 3), activation='relu', padding='same')(c7)
+
+    u8 = UpSampling2D((2, 2))(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(128, (3, 3), activation='relu', padding='same')(u8)
+    c8 = Conv2D(128, (3, 3), activation='relu', padding='same')(c8)
+
+    u9 = UpSampling2D((2, 2))(c8)
+    u9 = concatenate([u9, c1])
+    c9 = Conv2D(64, (3, 3), activation='relu', padding='same')(u9)
+    c9 = Conv2D(64, (3, 3), activation='relu', padding='same')(c9)
+
+    outputs = Conv2D(num_classes, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
+
+# Example usage
+if __name__ == "__main__":
+    model = unet()
+    model.summary()
